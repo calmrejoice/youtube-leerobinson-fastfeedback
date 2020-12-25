921ad808c31195d3d86d8c3a7596bbccdea0f44d
Sim <simtj1990@gmail.com>
2020-12-25 16:45:01 +0800

    Testing 2nd fix to api sites route

--- pages/api/sites.js
+++ pages/api/sites.js
@@ -1,4 +1,17 @@
-const db = require("@/lib/firebase-admin");
+const admin = require("firebase-admin");
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      client_email: process.env.FIREBASE_CLIENT_EMAIL,
+      private_key: process.env.FIREBASE_PRIVATE_KEY,
+      project_id: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    }),
+    databaseURL: "https://leerobinson-fastfeedback.firebaseio.com",
+  });
+}
+
+const db = admin.firestore();
 
 export default async (req, res) => {
   const snapshot = await db.collection("sites").get();
